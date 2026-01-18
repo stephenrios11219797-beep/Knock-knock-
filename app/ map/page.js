0d2ad2d316414c3f300e342aab37fad45f3b6087
stephenrios11219797-beep <stephenrios11219797@icloud.com>
2026-01-18 10:17:25 -0500

    Initialize Mapbox map page

--- app/ map/page.js
+++ app/ map/page.js
@@ -1,9 +1,26 @@
 'use client';
 
+import { useEffect } from 'react';
+import mapboxgl from 'mapbox-gl';
+
+mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_TOKEN;
+
 export default function MapPage() {
+  useEffect(() => {
+    const map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/streets-v12',
+      center: [-98.5795, 39.8283], // USA
+      zoom: 4
+    });
+
+    return () => map.remove();
+  }, []);
+
   return (
-    <div style={{ height: '100vh', width: '100%' }}>
-      <h1>Map Loading…</h1>
-    </div>
+    <div
+      id="map"
+      style={{ width: '100vw', height: '100vh' }}
+    />
   );
 }
