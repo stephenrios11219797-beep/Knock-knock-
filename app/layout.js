218f076095a5280fdb69aa08ba8301b0501fa093
stephenrios11219797-beep <stephenrios11219797@icloud.com>
2026-01-18 11:15:59 -0500

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -2,16 +2,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <nav style={{ padding: 12, borderBottom: "1px solid #ccc" }}>
-          <a href="/" style={{ marginRight: 12 }}>Home</a>
-          <a href="/rep" style={{ marginRight: 12 }}>Rep</a>
-          <a href="/manager">Manager</a>
-        </nav>
-
-        <main style={{ padding: 12 }}>
-          {children}
-        </main>
+        {children}
       </body>
     </html>
-  );
+  )
 }
