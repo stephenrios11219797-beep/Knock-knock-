6854745d0ce464f8eb29d817dd6fded47094eee0
stephenrios11219797-beep <stephenrios11219797@icloud.com>
2026-01-18 21:09:12 -0500

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,8 +1,3 @@
 export default function Home() {
-  return (
-    <main style={{ padding: 20 }}>
-      <h1>It works 🎉</h1>
-      <p>Root page loaded correctly.</p>
-    </main>
-  );
+  return <h1>It works</h1>
 }
