92e81476d28ada34cfb8466a009c59f8089ba34c
stephenrios11219797-beep <stephenrios11219797@icloud.com>
2026-01-18 23:36:00 -0500

    add shared components folder and header

---
+++ components/Header.js
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header style={{ padding: "16px", borderBottom: "1px solid #ddd" }}>
+      <h2>Knock Knock, Who’s There</h2>
+    </header>
+  );
+}
