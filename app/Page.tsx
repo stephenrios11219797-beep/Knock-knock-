03b64a91c9c9ae662ac0d7827916d83e212f2b5e
stephenrios11219797-beep <stephenrios11219797@icloud.com>
2026-01-18 20:44:08 -0500

    Delete app directory

--- app/Page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ padding: 20 }}>
-      <h1>Knock Knock</h1>
-      <p>App is live.</p>
-      <a href="/map">Go to Map</a>
-    </main>
-  );
-}
