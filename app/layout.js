9fff3b61e35f0518445f64b7ac720788c4d225d3
stephenrios11219797-beep <stephenrios11219797@icloud.com>
2026-01-18 22:25:42 -0500

    add persistent layout

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,18 @@
+export const metadata = {
+  title: 'Knock Knock',
+  description: 'Door to door sales app',
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <header>
+          <strong>Knock Knock</strong>
+        </header>
+
+        <main>{children}</main>
+      </body>
     </html>
   );
 }
