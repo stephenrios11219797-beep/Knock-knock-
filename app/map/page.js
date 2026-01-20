c51d4d625d32aff38de52ff2b9cdb73cfad871bb
stephenrios11219797-beep <stephenrios11219797@icloud.com>
2026-01-19 23:04:05 -0500

    Update page.js

--- app/map/page.js
+++ app/map/page.js
@@ -1,5 +1,9 @@
-import MapClient from './MapClient'
+import MapClient from "./MapClient";
 
 export default function MapPage() {
-  return <MapClient />
+  return (
+    <main style={{ width: "100%", height: "100vh" }}>
+      <MapClient />
+    </main>
+  );
 }
