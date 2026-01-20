967bfd628c35b81e25c0475c8fcf658359096cf1
stephenrios11219797-beep <stephenrios11219797@icloud.com>
2026-01-20 01:13:14 -0500

    Update MainNav.js

--- components/MainNav.js
+++ components/MainNav.js
@@ -1,27 +1,37 @@
-'use client'
+"use client";
 
-import Link from 'next/link'
-import { usePathname } from 'next/navigation'
+import { useRouter } from "next/navigation";
 
 export default function MainNav() {
-  const pathname = usePathname()
-
-  const linkClass = (path) =>
-    `px-4 py-2 rounded ${
-      pathname === path
-        ? 'bg-black text-white'
-        : 'bg-gray-200 text-black'
-    }`
+  const router = useRouter();
 
   return (
-    <nav className="flex gap-4 p-4 border-b">
-      <Link href="/" className={linkClass('/')}>
-        Home
-      </Link>
-
-      <Link href="/map" className={linkClass('/map')}>
-        Map
-      </Link>
-    </nav>
-  )
+    <div
+      style={{
+        position: "fixed",
+        top: 0,
+        left: 0,
+        right: 0,
+        height: 56,
+        background: "#111827",
+        display: "flex",
+        alignItems: "center",
+        padding: "0 16px",
+        zIndex: 20,
+      }}
+    >
+      <button
+        onClick={() => router.push("/")}
+        style={{
+          background: "transparent",
+          color: "white",
+          border: "none",
+          fontSize: 16,
+          fontWeight: 600,
+        }}
+      >
+        ← Home
+      </button>
+    </div>
+  );
 }
