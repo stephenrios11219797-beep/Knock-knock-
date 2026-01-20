fae741462ed4905644275fa696334eb62582c80b
stephenrios11219797-beep <stephenrios11219797@icloud.com>
2026-01-19 21:28:21 -0500

    Add map page with blank Mapbox map

--- app/map/page.js
+++ app/map/page.js
@@ -1,8 +1,22 @@
+'use client'
+
+import Map, { NavigationControl } from 'react-map-gl'
+import 'mapbox-gl/dist/mapbox-gl.css'
+
 export default function MapPage() {
   return (
-    <main>
-      <h1>Map Page</h1>
-      <p>If you see this, routing works.</p>
-    </main>
-  );
+    <div style={{ height: '100vh', width: '100%' }}>
+      <Map
+        mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_TOKEN}
+        initialViewState={{
+          longitude: -96,
+          latitude: 37.8,
+          zoom: 4
+        }}
+        mapStyle="mapbox://styles/mapbox/streets-v11"
+      >
+        <NavigationControl position="bottom-right" />
+      </Map>
+    </div>
+  )
 }
