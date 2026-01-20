cd8f6d9efd895eb6bad5ef05c776bc0c8e6d3db5
stephenrios11219797-beep <stephenrios11219797@icloud.com>
2026-01-19 23:28:16 -0500

    Update MapClient.js

--- app/map/MapClient.js
+++ app/map/MapClient.js
@@ -1,9 +1,9 @@
-"use client";
+'use client';
 
-import { useEffect, useRef } from "react";
-import mapboxgl from "mapbox-gl";
+import { useEffect, useRef } from 'react';
+import mapboxgl from 'mapbox-gl';
 
-mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN;
+mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_TOKEN;
 
 export default function MapClient() {
   const mapContainerRef = useRef(null);
@@ -13,9 +13,29 @@ export default function MapClient() {
 
     const map = new mapboxgl.Map({
       container: mapContainerRef.current,
-      style: "mapbox://styles/mapbox/streets-v12",
-      center: [-96, 37.8],
-      zoom: 4
+      style: 'mapbox://styles/mapbox/satellite-streets-v12',
+      center: [-96.7969, 32.7763], // change to your area if needed
+      zoom: 17,
+      pitch: 60,
+      bearing: -20,
+      antialias: true
+    });
+
+    map.on('load', () => {
+      map.addLayer({
+        id: '3d-buildings',
+        source: 'composite',
+        'source-layer': 'building',
+        filter: ['==', 'extrude', 'true'],
+        type: 'fill-extrusion',
+        minzoom: 15,
+        paint: {
+          'fill-extrusion-color': '#aaa',
+          'fill-extrusion-height': ['get', 'height'],
+          'fill-extrusion-base': ['get', 'min_height'],
+          'fill-extrusion-opacity': 0.6
+        }
+      });
     });
 
     return () => map.remove();
@@ -24,7 +44,7 @@ export default function MapClient() {
   return (
     <div
       ref={mapContainerRef}
-      style={{ width: "100%", height: "100vh" }}
+      style={{ width: '100%', height: '100vh' }}
     />
   );
 }
