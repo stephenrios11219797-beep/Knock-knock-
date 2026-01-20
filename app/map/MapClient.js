7f1918641190c2c78ed85a4e11e333ffbc449f59
stephenrios11219797-beep <stephenrios11219797@icloud.com>
2026-01-19 23:02:02 -0500

    Update MapClient.js

--- app/map/MapClient.js
+++ app/map/MapClient.js
@@ -1,29 +1,30 @@
-'use client'
+"use client";
 
-import { useEffect, useRef } from 'react'
-import mapboxgl from 'mapbox-gl'
-import 'mapbox-gl/dist/mapbox-gl.css'
+import { useEffect, useRef } from "react";
+import mapboxgl from "mapbox-gl";
+
+mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN;
 
 export default function MapClient() {
-  const mapContainerRef = useRef(null)
+  const mapContainerRef = useRef(null);
 
   useEffect(() => {
-    mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_TOKEN
+    if (!mapContainerRef.current) return;
 
     const map = new mapboxgl.Map({
       container: mapContainerRef.current,
-      style: 'mapbox://styles/mapbox/streets-v12',
-      center: [-77.0369, 38.9072], // DC for now
-      zoom: 12,
-    })
+      style: "mapbox://styles/mapbox/streets-v12",
+      center: [-96, 37.8],
+      zoom: 4
+    });
 
-    return () => map.remove()
-  }, [])
+    return () => map.remove();
+  }, []);
 
   return (
     <div
       ref={mapContainerRef}
-      style={{ width: '100%', height: '100vh' }}
+      style={{ width: "100%", height: "100vh" }}
     />
-  )
+  );
 }
