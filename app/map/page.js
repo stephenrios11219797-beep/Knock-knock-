45af434b525fb0130c9ae3780352f63b01967c7a
stephenrios11219797-beep <stephenrios11219797@icloud.com>
2026-01-20 01:09:28 -0500

    Update page.js

--- app/map/page.js
+++ app/map/page.js
@@ -1,9 +1,11 @@
 import MapClient from "./MapClient";
+import MainNav from "../../components/MainNav";
 
 export default function MapPage() {
   return (
-    <main style={{ width: "100%", height: "100vh" }}>
+    <div style={{ height: "100vh", width: "100%" }}>
+      <MainNav />
       <MapClient />
-    </main>
+    </div>
   );
 }
