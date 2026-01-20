1a1dc7a4e27836a32e375c43b7c02f2614b9a4b6
stephenrios11219797-beep <stephenrios11219797@icloud.com>
2026-01-19 21:52:33 -0500

    Update page.js

--- app/map/page.js
+++ app/map/page.js
@@ -1,22 +1,5 @@
-'use client'
-
-import Map, { NavigationControl } from 'react-map-gl'
-import 'mapbox-gl/dist/mapbox-gl.css'
+import MapClient from './MapClient'
 
 export default function MapPage() {
-  return (
-    <div style={{ height: '100vh', width: '100%' }}>
-      <Map
-        mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_TOKEN}
-        initialViewState={{
-          longitude: -96,
-          latitude: 37.8,
-          zoom: 4
-        }}
-        mapStyle="mapbox://styles/mapbox/streets-v11"
-      >
-        <NavigationControl position="bottom-right" />
-      </Map>
-    </div>
-  )
+  return <MapClient />
 }
