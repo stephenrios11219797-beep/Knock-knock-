2c9b9582a8693b0fa5531e81f6ea0cbfba12f58f
stephenrios11219797-beep <stephenrios11219797@icloud.com>
2026-01-20 22:25:26 -0500

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,7 +1,20 @@
-export default function Page() {
+"use client";
+
+import MainNav from "../components/MainNav";
+
+export default function HomePage() {
   return (
-    <main>
-      <h1>It works</h1>
-    </main>
+    <>
+      <MainNav />
+
+      {/* Home screen content (currently empty by design) */}
+      <div
+        style={{
+          height: "100vh",
+          width: "100%",
+          background: "#ffffff",
+        }}
+      />
+    </>
   );
 }
